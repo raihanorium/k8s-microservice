3b9255800da8e33028c3080d34dbe08905b196a6
Raihan <raihan.dk@gmail.com>
2024-09-26 00:46:42 +0600

    Project structure

---
+++ user-manager-api/src/main/java/com/raihanorium/usermanagerapi/UserManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.raihanorium.usermanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagerApiApplication.class, args);
+    }
+
+}
